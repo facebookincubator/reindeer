78f4a9fc4cd8983174acbc9ac1ffee79b993aeed
Andres Suarez <asuarez@meta.com>
2023-04-19 15:07:02 -0700

    Add trace measure for srcfiles
    Reviewed By: dtolnay
    
    Differential Revision: D45128962
    
    fbshipit-source-id: 2bea5c97ce7a6abaefebe936bfbc2b0b26393b77

--- src/buckify.rs
+++ src/buckify.rs
@@ -230,6 +230,7 @@ fn generate_target_rules<'scope>(
     // If we're configured to get precise sources and we're using 2018+ edition source, then
     // parse the crate to see what files are actually used.
     let mut srcs = if fixups.precise_srcs() && edition >= Edition::Rust2018 {
+        measure_time::trace_time!("srcfiles for {}", pkg);
         match srcfiles::crate_srcfiles(&tgt.src_path) {
             Ok(srcs) => {
                 let srcs = srcs
